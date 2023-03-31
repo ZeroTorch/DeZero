7671a479a707109a8d64246e26fbd0f26857f983
KorJYJ <tmspr3693@naver.com>
2023-03-31 16:56:52 +0900

    .

---
+++ step10.py
@@ -0,0 +1,29 @@
+import unittest
+import numpy as np
+from function import Variable, square
+
+class SquareTest(unittest.TestCase):
+    """
+    unittest.TestCase를 상속 받아서 만들고
+    
+    메서드 이름은 항상 test~ 로시작해야함
+
+    Args:
+        unittest (_type_): _description_
+    """
+    
+    def test_foward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+        
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad,expected)
+        
+        
+unittest.main() #를 입력하면 'step10.py'만 입력해도 test할 수 있음
